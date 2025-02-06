5b1114860f7b436385c0ac7be44a110c81cae81f
VolodymyrNahornyi <vnagornyi1989@gmail.com>
2025-02-06 12:09:54 +0200

    Removed Unused Imports

--- src/app/container/book-detail/book-detail.component.ts
+++ src/app/container/book-detail/book-detail.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CurrencyPipe, DatePipe, NgClass, NgForOf, NgIf, NgStyle} from "@angular/common";
 import {Book} from "../../model/book.model";
 import {SetBackgroundDirective} from "../../directives/set-background.directive";
