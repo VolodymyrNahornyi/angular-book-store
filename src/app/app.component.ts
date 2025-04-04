223fc41a5199713ecabf209928cfe0664effddf3
VolodymyrNahornyi <vnagornyi1989@gmail.com>
2025-04-04 12:43:44 +0300

    Removed Unused Links for Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import {RouterModule, RouterOutlet} from '@angular/router';
+import {RouterModule} from '@angular/router';
 import {HeaderComponent} from "./header/header.component";
-import {ContainerComponent} from "./container/container.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, HeaderComponent, ContainerComponent],
+  imports: [RouterModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
