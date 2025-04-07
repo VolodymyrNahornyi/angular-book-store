ec126b3b245517b6b6a8c77dd8253cc6c58551d8
VolodymyrNahornyi <vnagornyi1989@gmail.com>
2025-04-07 14:20:31 +0300

    Added Page Not Found Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import {ContainerComponent} from "./container/container.component";
 import {AboutComponent} from "./about/about.component";
 import {ContactsComponent} from "./contacts/contacts.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
 
 export const routes: Routes = [
   {path: '', redirectTo: 'Books', pathMatch: 'full'},
   {path: 'Books', component: ContainerComponent},
   {path: 'About', component: AboutComponent},
-  {path: 'Contacts', component: ContactsComponent}
+  {path: 'Contacts', component: ContactsComponent},
+  {path: '**', component: NotFoundComponent}
 ];
