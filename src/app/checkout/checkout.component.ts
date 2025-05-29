0687751f2b21cfe0c3c817a3227b7ba6f560e7cb
VolodymyrNahornyi <vnagornyi1989@gmail.com>
2025-05-29 11:36:53 +0300

    Passed Data By Route

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {CurrencyPipe, NgClass, NgIf} from "@angular/common";
+import {Router} from "@angular/router";
+import {Book} from "../model/book.model";
 
 @Component({
   selector: 'app-checkout',
@@ -12,7 +14,14 @@ import {CurrencyPipe, NgClass, NgIf} from "@angular/common";
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css'
 })
-export class CheckoutComponent {
+export class CheckoutComponent implements OnInit {
+
+  router: Router = inject(Router);
+  book: Book = {} as Book;
+
+  ngOnInit(): void {
+    this.book = history.state;
+  }
 
   getTax(price: number): number {
     return price * 0.1; // Розрахунок податку як 10% від ціни
